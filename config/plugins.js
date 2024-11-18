e11706194a917dc1a4f2b20d194840033ba7c752
Jorj Hambaryan <jorjhambaryan@MacBookPro.lan>
2024-11-18 17:41:31 +0400

    emial sender

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "Info@eventcage.com",
+          pass: env("EMAIL_TOKEN"),
+        },
+      },
+      settings: {
+        defaultFrom: "Info@eventcage.com",
+        defaultReplyTo: "Info@eventcage.com",
+      },
+    },
+  },
+});
