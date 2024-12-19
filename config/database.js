adf7b781d63dcb1885eac456ab0940aadc47a714
Jorj Hambaryan <jorjhambaryan@Jorjs-MacBook-Pro.local>
2024-12-19 21:24:50 +0400

    change pools

--- config/database.js
+++ config/database.js
@@ -31,7 +31,8 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "%"),
+        // host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
